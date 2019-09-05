00ec792370e29511526d330b18f6b865bcd2bce5
xbowtie <54191599+xbowtie@users.noreply.github.com>
2019-09-06 00:55:24 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -3,7 +3,7 @@
 
 start(){
 	clear
-	echo -e "\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mCode: \e[37mYaman Efkar\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mİnstagram: \e[37myamanefkarr\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mYoutube: \e[37mYaman Efkar\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mGitHub: \e[37myamanefkar\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  Tool Name: Wordlist-Pluas\n\n\n"
+	echo -e "\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mCode: \e[37mxBowtie\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄ 
 
 
 
@@ -125,7 +125,7 @@ else
 apt install php -y 
 clear
 cd Lib/
-echo "Bu Tool YamanEfkar Tarafından Kodlandı...." >> ok.txt
+echo "Bu Tool xBowtie Tarafından Kodlandı...." >> ok.txt
 cd ..
 bash tst.sh	
 fi
